ccbf2b2c7dca4cc4d58a04db769e82499a835305
PPTaa <chobo05@naver.com>
2021-01-07 23:47:08 +0900

    album app basic

--- AlbumApp/ViewController.swift
+++ AlbumApp/ViewController.swift
@@ -8,26 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var randPrice = 0
+    // 금액 text
+    @IBOutlet weak var priceLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        refreshNum()
     }
     
-    @IBAction func randomNumberGenerator(_ sender: Any) {
-        let randint = Int.random(in: 1...10)
-        print(randint)
-        
-        let alert = UIAlertController(title: "Random Number", message: "Random Number is "+String(randint), preferredStyle: .alert)
-        let alertOK = UIAlertAction(title: "Good", style: .default, handler: nil)
+    @IBAction func randomPriceGenerator(_ sender: Any) {
+        let message = "가격은 \(randPrice)원 입니다"
+        let alert = UIAlertController(title: "Random Number", message: message, preferredStyle: .alert)
+        // handler 에 들어간 부분이 closer : 실행가능 한 코드블럭
+        // alertOK가 눌리면 handler의 코드를 실행해라.
+        let alertOK = UIAlertAction(title: "Good", style: .default, handler: { action in
+            self.refreshNum()
+        })
         
         alert.addAction(alertOK)
-        
         present(alert, animated: true, completion: nil)
         
     }
+    // 중복 코드 메소드화
+    func refreshNum(){
+        let randInt = Int.random(in: 10000...100000)
+        randPrice = randInt
+        priceLabel.text = "금액 : \(randPrice)원"
+    }
     
-    // test
-    
+    @IBOutlet weak var myImge: UIImageView!
 }
 
