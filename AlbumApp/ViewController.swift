ecd91f2271ab8ea6854b7b928c2736008641aba8
PPTaa <chobo05@naver.com>
2021-01-07 22:09:13 +0900

    App UI

---
+++ AlbumApp/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  AlbumApp
+//
+//  Created by leejungchul on 2021/01/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func randomNumberGenerator(_ sender: Any) {
+        let randint = Int.random(in: 1...10)
+        print(randint)
+        
+        let alert = UIAlertController(title: "Random Number", message: "Random Number is "+String(randint), preferredStyle: .alert)
+        let alertOK = UIAlertAction(title: "Good", style: .default, handler: nil)
+        
+        alert.addAction(alertOK)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+}
+
